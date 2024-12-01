76275a4ce9e85db253b6d500a9e6e60fb67991c3
potakaaa <helbirog@gmail.com>
2024-12-01 18:53:47 +0800

    added form control on signup

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -19,67 +19,66 @@ const SignupPage = () => {
     event.preventDefault();
 
     if (input.password === input.passwordconfirm) {
-      await supabase
-        .from("DIM_USER")
-        .insert({
-          STUDENT_ID: input.id,
-          USER_NAME: input.name,
-          USER_EMAIL: input.email,
-          USER_PASS: input.password,
-        });
+      await supabase.from("DIM_USER").insert({
+        STUDENT_ID: input.id,
+        USER_NAME: input.name,
+        USER_EMAIL: input.email,
+        USER_PASS: input.password,
+      });
     } else {
       alert("Failed confirmed password attempt!");
     }
   }
 
   return (
-    <div className="container">
-      <div className="column-container">
-        <div className="left-container">
-          <h1>TrailMarket</h1>
-          <h3>The exclusive market for USTP-CDO Students!</h3>
-        </div>
-        <div className="right-container">
-          <input
-            name="name"
-            type="text"
-            placeholder="Name"
-            className="name-input"
-            onChange={handleChange}
-          />
-          <input
-            name="id"
-            type="number"
-            placeholder="ID Number"
-            className="id-input"
-            onChange={handleChange}
-          />
-          <input
-            name="email"
-            type="email"
-            placeholder="Email"
-            className="email-input"
-            onChange={handleChange}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            className="password-input"
-            name="password"
-            onChange={handleChange}
-          />
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            className="confirm-password-input"
-            name="passwordconfirm"
-            onChange={handleChange}
-          />
-          <button className="login-button" onClick={handleSignUp}>
-            Sign up
-          </button>
-        </div>
-      </div>
+    <div className="right-container">
+      <input
+        name="name"
+        type="text"
+        placeholder="Name"
+        id="name-input"
+        className="form-control"
+        onChange={handleChange}
+      />
+      <input
+        name="id"
+        type="number"
+        placeholder="ID Number"
+        id="id-input"
+        className="form-control"
+        onChange={handleChange}
+      />
+      <input
+        name="email"
+        type="email"
+        placeholder="Email"
+        id="email-input"
+        className="form-control"
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        id="password-input"
+        className="form-control"
+        name="password"
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        placeholder="Confirm Password"
+        id="confirm-password-input"
+        className="form-control"
+        name="passwordconfirm"
+        onChange={handleChange}
+      />
+      <button
+        id="login-button"
+        className="btn btn-primary"
+        onClick={handleSignUp}
+      >
+        Sign up
+      </button>
     </div>
   );
 };
