4abf68a87851ab750f3034fcaf03397f762c43bc
unknown <delmundo.hansmatthew@gmail.com>
2024-12-01 18:56:33 +0800

    t

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -31,10 +31,6 @@ const SignupPage = () => {
   return (
     <div className="container">
       <div className="column-container">
-        <div className="left-container">
-          <h1>TrailMarket</h1>
-          <h3>The exclusive market for USTP-CDO Students!</h3>
-        </div>
         <div className="right-container">
           <input name="name" type="text" placeholder="Name" className="name-input" onChange={handleChange} />
           <input name="id" type="number" placeholder="ID Number" className="id-input" onChange={handleChange} />
