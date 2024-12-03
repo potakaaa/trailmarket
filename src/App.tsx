fe18c836ebcb5eff144ea78e856778e024c47571
potakaaa <helbirog@gmail.com>
2024-12-03 11:21:27 +0800

    created react router on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import SignOrLoginPage from "./pages/login_signup/SignOrLoginPage";
 import NavBar from "./pages/NavBar";
@@ -7,13 +7,24 @@ import "./App.css";
 import ProductPage from "./pages/ProductPage";
 import ForgetPass from "./pages/login_signup/ForgetPass";
 import LoginPage from "./pages/login_signup/LoginPage";
+import { useNavigate } from "react-router-dom";
+import { Sign } from "crypto";
 
 const App = () => {
-  return (
-    <div id="container" className="">
-      <SignOrLoginPage />
-    </div>
-  );
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const nav = useNavigate();
+
+  useEffect(() => {
+    // Redirect based on `isLoggedIn` state
+    if (isLoggedIn) {
+      nav("/home"); // Redirect to home
+    } else {
+      nav("/login"); // Redirect to login
+    }
+  }, [isLoggedIn, nav]);
+
+  return <div id="container" className=""></div>;
 };
 
 export default App;
