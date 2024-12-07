8d28959e390c2b60361eacd717c5ab8667076fea
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2024-12-08 00:15:53 +0800

    WORK IN PROGRESS

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import TopNavBar from './navbar/TopNavBar';
+import NavBar from './navbar/NavBar';
 const kanye = "https://media.gq.com/photos/5ad93798ceb93861adb912d8/16:9/w_2672,h_1503,c_limit/kanye-west-0814-GQ-FEKW01.01.jpg";
 
 const categories = ["Bags", "Accessories", "Clothes", "Gadgets"]
@@ -51,45 +53,40 @@ const SearchResults = () => {
 
 
     return (
-        <div className="app-wrapper flex flex-col items-center justify-start min-h-screen w-full overflow-y-visible px-3">
-
-            <div className='search-wrapper flex flex-col h-full w-full border-green-500 border-2 p-4 space-y-6'>
-
-                <div className='filter-sort flex flex-[1] bg-gray-50 h-full drop-shadow-xl rounded-xl p-8 z-10'>
-
-
-                    <Dropdown onSelect={handleSelect} options={categories}>Tags</Dropdown>
-                    <Dropdown onSelect={handleSelect} options={["High to Low", "Low to High"]}>Price</Dropdown>
-                    <Dropdown onSelect={handleSelect} options={["Latest to Oldest", "Oldest to Latest"]}>Date</Dropdown>
-
-
-                </div>
-                <div className='search-results flex flex-[3] bg-gray-50 drop-shadow-xl rounded-xl p-8 flex-col'>
-                    <h1>Search Results for "ITEM"</h1>
-                    <div className=' result-container w-full flex items-center'>
-
-                        <div className='w-56 space-y-3 flex flex-col'>
-                            <div className='flex flex-row align-middle space-x-2'>
-                                <div className="w-4 h-4 rounded-full overflow-hidden border border-black">
-                                    <img className='object-cover h-full w-full' src={kanye}></img>
+        <div className='flex flex-col'>
+            <TopNavBar/>
+            <NavBar/>
+            <div className="app-wrapper flex flex-col items-center justify-start min-h-screen w-full overflow-y-visible px-3">
+                <div className='search-wrapper flex flex-col h-full w-full space-y-6 md:flex-row space-x-2'>
+                    <div className='filter-sort flex flex-[1] bg-gray-50 h-full drop-shadow-xl rounded-xl p-8 z-10 md:z-0 justify-center'>
+                        <Dropdown onSelect={handleSelect} options={categories}>Tags</Dropdown>
+                        <Dropdown onSelect={handleSelect} options={["High to Low", "Low to High"]}>Price</Dropdown>
+                        <Dropdown onSelect={handleSelect} options={["Latest to Oldest", "Oldest to Latest"]}>Date</Dropdown>
+                    </div>
+                    <div className='search-results flex flex-[3] bg-gray-50 drop-shadow-xl rounded-xl p-8 flex-col space-y-3'>
+                        <h1>HAPPY BIRTHDAY</h1>
+                        <div className=' result-container w-full flex items-center justify-center md:grid md:grid-cols-3 md:gap-4'>
+                            <div className='PRODUCT-CONTAINER w-full space-y-3 flex flex-col'>
+                                <div className='flex flex-row align-middle space-x-2'>
+                                    <div className="w-4 h-4 rounded-full overflow-hidden border border-black">
+                                        <img className='object-cover h-full w-full' src={kanye}></img>
+                                    </div>
+                                    <p className='text-xs align-middle'>Username</p>
                                 </div>
-                                <p className='text-xs align-middle'>Username</p>
+            
+                                <div className='flex flex-[5] rounded-xl overflow-hidden aspect-square '>
+                                    <img src={kanye} className='object-cover'></img>
+                                </div>
+                                <div className='flex flex-[2] bg-zinc-900 rounded-xl p-4 flex-col'>
+                                <h2 className='text-white text-xl'>HAPPY BIRTHDAY</h2>
+                                <h2 className='text-white font-normal text-sm'>RALD</h2>
                             </div>
-                            
-
-                            <div className='flex flex-[5] rounded-xl overflow-hidden aspect-square '>
-                                <img src={kanye} className='object-cover'></img>
                             </div>
-                            <div className='flex flex-[2] bg-zinc-900 rounded-xl p-4 flex-col'>
-                            <h2 className='text-white text-xl'>4 Orders pending</h2>
-                            <h2 className='text-white font-normal text-sm'>Lorem Ipsum</h2>
+            
                         </div>
-                        </div>
-                        
                     </div>
                 </div>
             </div>
-
         </div>
 
 
