b881e367907542dab3b2421f278b52888baf56ea
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2024-12-08 08:02:30 +0800

    n

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -57,29 +57,29 @@ const SearchResults = () => {
             <TopNavBar/>
             <NavBar/>
             <div className="app-wrapper flex flex-col items-center justify-start min-h-screen w-full overflow-y-visible px-3">
-                <div className='search-wrapper flex flex-col h-full w-full space-y-6 md:flex-row space-x-2'>
-                    <div className='filter-sort flex flex-[1] bg-gray-50 h-full drop-shadow-xl rounded-xl p-8 z-10 md:z-0 justify-center'>
+                <div className='search-wrapper flex flex-col h-full w-full space-y-6 md:flex-row'>
+                    <div className='filter-sort flex flex-[1] bg-gray-50 h-full drop-shadow-xl rounded-xl p-8 z-10'>
                         <Dropdown onSelect={handleSelect} options={categories}>Tags</Dropdown>
                         <Dropdown onSelect={handleSelect} options={["High to Low", "Low to High"]}>Price</Dropdown>
                         <Dropdown onSelect={handleSelect} options={["Latest to Oldest", "Oldest to Latest"]}>Date</Dropdown>
                     </div>
                     <div className='search-results flex flex-[3] bg-gray-50 drop-shadow-xl rounded-xl p-8 flex-col space-y-3'>
-                        <h1>HAPPY BIRTHDAY</h1>
+                        <h1>Search Results for "ITEM"</h1>
                         <div className=' result-container w-full flex items-center justify-center md:grid md:grid-cols-3 md:gap-4'>
-                            <div className='PRODUCT-CONTAINER w-full space-y-3 flex flex-col'>
+                            <div className='PRODUCT-CONTAINER w-56 space-y-3 flex flex-col'>
                                 <div className='flex flex-row align-middle space-x-2'>
                                     <div className="w-4 h-4 rounded-full overflow-hidden border border-black">
                                         <img className='object-cover h-full w-full' src={kanye}></img>
                                     </div>
-                                    <p className='text-xs align-middle'>Username</p>
+                                    <p className='text-xs align-middle'>WORK ON PADDING WORK ON PADDING</p>
                                 </div>
             
                                 <div className='flex flex-[5] rounded-xl overflow-hidden aspect-square '>
                                     <img src={kanye} className='object-cover'></img>
                                 </div>
                                 <div className='flex flex-[2] bg-zinc-900 rounded-xl p-4 flex-col'>
-                                <h2 className='text-white text-xl'>HAPPY BIRTHDAY</h2>
-                                <h2 className='text-white font-normal text-sm'>RALD</h2>
+                                <h2 className='text-white text-xl'>4 Orders pending</h2>
+                                <h2 className='text-white font-normal text-sm'>Lorem Ipsum</h2>
                             </div>
                             </div>
             
