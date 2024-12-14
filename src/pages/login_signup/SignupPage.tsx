b2b81e0bd4f75b096996b14836cb73f219581ace
potakaaa <helbirog@gmail.com>
2024-12-14 15:23:19 +0800

    cleaning product page

--- src/pages/login_signup/SignupPage.tsx
+++ src/pages/login_signup/SignupPage.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { supabase } from "../../createClient";
 import { ChangeEvent } from "react";
-import "./SignupPage.css";
 import { useNavigate } from "react-router-dom";
 
 {
