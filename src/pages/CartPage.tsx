0d33a9f4d5b187ff8cfd772280c513adea19222d
potakaaa <helbirog@gmail.com>
2024-12-19 16:41:32 +0800

    cart shows cart is empty if null

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -197,71 +197,77 @@ const CartPage = () => {
       </div>
       <div className="CartBody flex flex-col h-full my-5">
         <div className="CartItemList flex flex-col xl:grid xl:grid-cols-1 2xl:grid-cols-2 gap-5">
-          {cart.map((item) => (
-            <button
-              key={item?.prod_id}
-              className="CartItem shadow-[0_0px_20px_rgba(0,0,0,0.2)] rounded-2xl flex flex-col md:flex-row items-stretch"
-              onClick={() => nav(`/product/${item?.prod_id}`)}
-            >
-              <div className="md:w-3/4 w-full h-full flex-1 flex flex-col">
-                <img
-                  src={item?.img}
-                  alt={item?.name}
-                  className="Image flex-1 rounded-2xl object-cover max-h-[250px] min-h-[250px]"
-                />
-              </div>
-              <div className="CartItemInfo p-6 flex flex-col md:w-3/4 w-full justify-center">
-                <div className="CartItemTop">
-                  <div className="CartItemSeller flex items-center w-full">
-                    <div className="CartItemSellerImage bg-gray-700 size-3 mr-2 rounded-full"></div>
-                    <p className="CartItemSellerName text-xs font-medium">
-                      {item?.seller}
-                    </p>
-                    <div className="flex justify-end flex-grow">
-                      <button
-                        className="flex justify-end self-end items-end p-2 hover:rounded-full hover:shadow-md bg-transparent transition-all duration-300"
-                        onClick={() => handleDelete(item?.prod_id)}
-                      >
-                        <BiTrash className="size-6 text-red-600 shadow-sm bg-transparent" />
-                      </button>
-                    </div>
-                  </div>
+          {!cart.length ? (
+            <h1 className="flex items-center justify-center text-xl">
+              No items in cart
+            </h1>
+          ) : (
+            cart.map((item) => (
+              <button
+                key={item?.prod_id}
+                className="CartItem shadow-[0_0px_20px_rgba(0,0,0,0.2)] rounded-2xl flex flex-col md:flex-row items-stretch"
+                onClick={() => nav(`/product/${item?.prod_id}`)}
+              >
+                <div className="md:w-3/4 w-full h-full flex-1 flex flex-col">
+                  <img
+                    src={item?.img}
+                    alt={item?.name}
+                    className="Image flex-1 rounded-2xl object-cover max-h-[250px] min-h-[250px]"
+                  />
                 </div>
-                <div className="CartItemMiddle my-2">
-                  <h1 className="text-2xl border-b-2 border-gray-400 pb-4 md:text-left sm:text-center">
-                    {item?.name}
-                  </h1>
-                  <div className="CartItemsPriceConfig flex pt-[15px] justify-between">
-                    <div className="CartItemsUnitPrice w-1/3">
-                      <p className="text-sm font-medium">Unit Price</p>
-                      <h1 className="text-2xl">{item?.price}</h1>
-                    </div>
-                    <div className="CartItemsQuantity w-1/3">
-                      <p className="text-sm font-medium">Quantity</p>
-                      <div className="CartItemsQuantityEdit">
-                        <input
-                          type="number"
-                          value={item?.quantity}
-                          min="1"
-                          readOnly
-                          onChange={(e) => {
-                            setQuantity(parseInt(e.target.value));
-                            console.log(quantity);
-                          }}
-                          className="w-2/3 border-[2px] border-black rounded-2xl text-center"
-                          onKeyDown={(e) => e.preventDefault()}
-                        />
+                <div className="CartItemInfo p-6 flex flex-col md:w-3/4 w-full justify-center">
+                  <div className="CartItemTop">
+                    <div className="CartItemSeller flex items-center w-full">
+                      <div className="CartItemSellerImage bg-gray-700 size-3 mr-2 rounded-full"></div>
+                      <p className="CartItemSellerName text-xs font-medium">
+                        {item?.seller}
+                      </p>
+                      <div className="flex justify-end flex-grow">
+                        <button
+                          className="flex justify-end self-end items-end p-2 hover:rounded-full hover:shadow-md bg-transparent transition-all duration-300"
+                          onClick={() => handleDelete(item?.prod_id)}
+                        >
+                          <BiTrash className="size-6 text-red-600 shadow-sm bg-transparent" />
+                        </button>
                       </div>
                     </div>
-                    <div className="CartItemsTotalPrice w-1/3">
-                      <p className="text-sm font-medium">Total Price</p>
-                      <h1 className="text-2xl">{totalAmount}</h1>
+                  </div>
+                  <div className="CartItemMiddle my-2">
+                    <h1 className="text-2xl border-b-2 border-gray-400 pb-4 md:text-left sm:text-center">
+                      {item?.name}
+                    </h1>
+                    <div className="CartItemsPriceConfig flex pt-[15px] justify-between">
+                      <div className="CartItemsUnitPrice w-1/3">
+                        <p className="text-sm font-medium">Unit Price</p>
+                        <h1 className="text-2xl">{item?.price}</h1>
+                      </div>
+                      <div className="CartItemsQuantity w-1/3">
+                        <p className="text-sm font-medium">Quantity</p>
+                        <div className="CartItemsQuantityEdit">
+                          <input
+                            type="number"
+                            value={item?.quantity}
+                            min="1"
+                            readOnly
+                            onChange={(e) => {
+                              setQuantity(parseInt(e.target.value));
+                              console.log(quantity);
+                            }}
+                            className="w-2/3 border-[2px] border-black rounded-2xl text-center"
+                            onKeyDown={(e) => e.preventDefault()}
+                          />
+                        </div>
+                      </div>
+                      <div className="CartItemsTotalPrice w-1/3">
+                        <p className="text-sm font-medium">Total Price</p>
+                        <h1 className="text-2xl">{totalAmount}</h1>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </button>
-          ))}
+              </button>
+            ))
+          )}
         </div>
       </div>
     </div>
